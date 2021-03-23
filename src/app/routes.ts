080c181f20d26c9fd12bca78ad14c0dfd24e472b
Alexamith <samirportillo231099@gmail.com>
2021-03-23 15:51:32 -0600

    Bloqueo de rutas

--- src/app/routes.ts
+++ src/app/routes.ts
@@ -4,11 +4,12 @@ import { NgModule } from '@angular/core';
 import { PreciosComponent } from './components/precios/precios.component';
 import { HomeComponent } from './components/home/home.component';
 import { ProtegidaComponent } from './components/protegida/protegida.component';
+import { AuthGuard } from './services/auth.guard';
 
 const routes: Routes = [
     { path: 'home', component: HomeComponent },
     { path: 'precios', component: PreciosComponent },
-    { path: 'protegida', component: ProtegidaComponent },
+    { path: 'protegida', component: ProtegidaComponent, canActivate:[AuthGuard] },
     { path: '**', pathMatch:'full',redirectTo:'home' },
 ];
 
